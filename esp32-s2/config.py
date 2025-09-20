805500de8c5d76151b4e83fa962a45d907394027
mekatrol <git@mekatrol.com>
2025-09-20 10:15:52 +1000

    mqtt config

--- esp32-s2/config.py
+++ esp32-s2/config.py
@@ -1,5 +1,9 @@
 config = {
     "wlan_ssid": "**SSID**",
     "wlan_pwd": "**PASSWORD**",
+    "mqtt_host": "mqtt.lan",
+    "mqtt_username": "**USER**",
+    "mqtt_password": "**PASSWORD**",
+    "mqtt_clientid": "**CLIENT_ID**",
     "time_server": "au.pool.ntp.org",
 }
